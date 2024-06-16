95a0812cfc9c7d4caa56a0ffc491a7cb0ee5059a
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-06-16 10:36:25 -0300

    criando alguns slash_cammand

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,31 @@
+use std::collections::HashMap;
+use std::sync::Mutex;
 use poise::serenity_prelude as serenity;
 
-struct Data {} // User data, which is stored and accessible in all command invocations
+pub mod slash_command;
+
+pub struct Data {
+    votes: Mutex<HashMap<String, u32>>,
+}
+// User data, which is stored and accessible in all command invocations
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-
-/// Displays your or another user's account creation date
-#[poise::command(slash_command, prefix_command)]
-async fn age(
-    ctx: Context<'_>,
-    #[description = "Selected user"] user: Option<serenity::User>,
-) -> Result<(), Error> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
-    let response = format!("{}'s account was created at {}", u.name, u.created_at());
-    ctx.say(response).await?;
-    Ok(())
-}
 
 #[tokio::main]
 async fn main() {
     let token = std::env::var("DISCORD_TOKEN").expect("missing DISCORD_TOKEN");
-    let intents = serenity::GatewayIntents::non_privileged();
+    let intents = serenity::GatewayIntents::all();
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
-            commands: vec![age()],
+            commands: vec![slash_command::age_command::age(),slash_command::details_command::getvotes(),slash_command::details_command::help(),
+                           slash_command::details_command::vote(), slash_command::life_command::life()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(Data {votes: Mutex::new(HashMap::new())})
             })
         })
         .build();
