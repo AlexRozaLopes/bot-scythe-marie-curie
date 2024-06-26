5c3e5148b56815cd8f34a148d27870cd71c85bc0
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-06-26 08:00:14 -0300

    dm

--- src/event_handle/fun_handle.rs
+++ src/event_handle/fun_handle.rs
@@ -1,5 +1,5 @@
 use poise::serenity_prelude as serenity;
-use serenity::all::Message;
+use serenity::all::{Colour, CreateEmbed, CreateMessage, GuildId, Message};
 
 use crate::{Data, Error};
 
@@ -9,7 +9,7 @@ pub async fn dont_say_this_name(
     new_message: &Message,
     data: &Data,
 ) -> Result<(), Error> {
-    let ban_words = { data.ban_words.lock().unwrap().get(&new_message.guild_id.unwrap()).unwrap_or(&vec!["voldemort".to_string()]).clone() };
+    let ban_words = { data.ban_words.lock().unwrap().get(&new_message.guild_id.unwrap_or(GuildId::new(1))).unwrap_or(&vec!["voldemort".to_string()]).clone() };
     for bn in ban_words {
         if new_message.content.to_lowercase().contains(&bn)
             && new_message.author.id != ctx.cache.current_user().id
@@ -21,6 +21,16 @@ pub async fn dont_say_this_name(
                 )
                 .await?;
             new_message.delete(ctx).await?;
+
+            let intro_description = "Recentemente, recebemos relatos e observamos comportamentos que não estão de acordo com os padrões de conduta esperados neste servidor.".to_string();
+            let motivo_description = format!("Esse incidente ocorreu devido esta mensagem em particular: ====> {}", new_message.content);
+            let intro = CreateEmbed::new().title("Conduta Inapropriada").description(intro_description).color(Colour::RED);
+            let motivo = CreateEmbed::new().title("Principal Motivo").description(motivo_description).color(Colour::RED);
+            let message_into = CreateMessage::new().add_embed(intro);
+            let message_motivo = CreateMessage::new().add_embed(motivo);
+
+            new_message.author.direct_message(ctx, message_into).await?;
+            new_message.author.direct_message(ctx, message_motivo).await?;
         }
     }
 
