6042d7741a1cae300621d15aab64fc41f2bdd4e6
AlexRozaLopes <alexrozaestudos@gmail.com>
2024-07-23 12:27:03 -0300

    add terraform para deploy fix

---
+++ deploy.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = env("AWS_ACCESS_KEY_ID")
+  secret_key = env("AWS_SECRET_ACCESS_KEY")
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-074be47313f84fa38"
+  instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              service docker start
+              usermod -a -G docker ec2-user
+              docker network create net-bot
+              docker run -d -p 6379:6379 -p 8001:8001 --name redis-bot --network net-bot redis/redis-stack
+              docker run -d --name bot-scythe --network net-bot alexroza/bot-scythe-marie-curie
+
+              EOF
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
